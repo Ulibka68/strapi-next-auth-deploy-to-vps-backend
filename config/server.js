fb89610f6a6f5f2efbe2c0fb3feb7bfd23619e08
Ulibka68 <gena68@inbox.ru>
2021-08-10 23:39:18 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7bc76fb512929a59dd261142b827073e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9706297f-aede-4bde-8895-7a3c51400d12'
+};
